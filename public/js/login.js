797c66607010e88741f178056b45c51a3ab68685
EarlofSmith <r1smith3333@gmail.com>
2023-02-07 09:59:42 -0700

    added the signUP code to the login.js

--- public/js/login.js
+++ public/js/login.js
@@ -19,4 +19,28 @@ const login = async (event) => {
     }
   };
   
+  const signUp = async (event) => {
+    event.preventDefault();
+  
+    const first_name = document.querySelector("#first-name-signup").value.trim();
+    const last_name = document.querySelector("#last-name-signup").value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
+
+    if (first_name && last_name && email && password) {
+        const response = await fetch("/api/user", {
+            method: "POST",
+            body: JSON.stringify({ first_name, last_name, email, password }),
+            headers: { "Content-Type": "application/json" },
+        });
+        
+        if (response.ok) {
+            document.location.replace("/login");
+        } else {
+            alert("Error creating user");
+        }
+    }
+  };
+
   document.querySelector('.login-form').addEventListener('submit', login);
+  document.querySelector('.signup-form').addEventListener('submit', signUp );
