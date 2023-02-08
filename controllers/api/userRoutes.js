a159bcbe76e601b62d1feaa955bd61505ce8883d
EarlofSmith <r1smith3333@gmail.com>
2023-02-08 16:08:18 -0700

    added a logout.js

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -49,4 +49,15 @@ router.post ('/', async (req, res) => {
     }
 });
 
+// log out
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+      req.session.destroy(() => {
+        res.status(204).end();
+      });
+    } else {
+      res.status(404).end();
+    }
+  });
+
 module.exports = router;
